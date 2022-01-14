ae1b60828e53820a2c136227cd556d94ec5b776d
Hungyuuki <hungyuuki777@gmail.com>
2022-01-14 14:51:49 +0700

    2

--- src/com/codegym/SimpleCalculator.java
+++ src/com/codegym/SimpleCalculator.java
@@ -2,7 +2,8 @@ package com.codegym;
 
 public class SimpleCalculator {
     public static int add(int first, int second){
-        return first + second;
+//        return first + second;
+        throw new UnsupportedOperationException("not support add method");
     }
 
     public static int sub(int first, int second){
